b3776a0b61c1f1b166989756614431211f3a4b8d
asifandhi <asifandhi267@gmail.com>
2026-04-13 18:11:24 +0530

    1st feat: add SVG assets for React and Vite, configuration file, and main entry point
    - Added react.svg and vite.svg to the assets directory.
    - Created a configuration file (conf.js) for Appwrite settings.
    - Introduced index.css for global styles and theming.
    - Set up main.jsx as the entry point for the React application.
    - Configured Vite with the React plugin in vite.config.js.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+ 
+import './App.css'
+import conf from './conf/conf'
+
+function App() {
+
+  
+  return (
+   <>
+    <h1>Meha blog app</h1>
+   </>
+  )
+}
+
+export default App
