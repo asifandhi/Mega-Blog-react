37d80347c176300bc482b9ef203791a93ee0787b
asifandhi <asifandhi267@gmail.com>
2026-05-09 14:46:32 +0530

    feat: add Signup and Protected components for user authentication flow

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,25 @@
+import React ,{useState,useEffect} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected({children,authentication = true}) {
+    const navigate = useNavigate();
+    const [loader,setLoader] = useState(true);
+    const authStatus = useSelector(state => state.auth.isAuthenticated);
+
+    useEffect(() => {
+        if(authentication && authStatus !== authentication){
+            navigate("/login");
+        }
+        else if (!authentication && authStatus !== authentication){
+            navigate("/");
+        }
+        setLoader(false);
+    },[authentication,authStatus,navigate])
+  return loader ? <div className='flex items-center justify-center w-full h-screen'>
+    <p className='text-xl text-gray-500'>Loading...</p>
+    </div> : <>{children}</>
+
+}
+
+ 
