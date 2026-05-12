ca5984e5b8331581b5b8fd16d7f14c307d8f93ea
asifandhi <asifandhi267@gmail.com>
2026-05-12 08:22:04 +0530

    feat: implement routing and components for post management and authentication

--- src/components/index.js
+++ src/components/index.js
@@ -4,10 +4,15 @@ import Footer from "./footer/Footer";
 import Container from "./container/container";
 import Logo from "./logo";
 import Logout from "./header/Logout";
+import Login from './Login'
 import Button from "./Button";
 import Input from './Input';
 import RTE from "./RTE";
 import Select from './Select'
+import PostForm from "./post-form/PostForm";
+import Postcard from "../components/Postcard";
+import AuthLayout from '../components/AuthLayout';
+
 export{
     Header,
     Footer,
@@ -16,6 +21,12 @@ export{
     Logout,
     Button,
     Input,
-    RTE
+    RTE,
+    Select,
+    Login,
+    PostForm,
+    Postcard,
+    AuthLayout
+
     
 }
